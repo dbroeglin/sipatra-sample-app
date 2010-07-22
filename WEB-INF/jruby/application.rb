3cc1ab980f26a37b222673d991e6c1d7029cd613
Dominique Broeglin <dominique.broeglin@gmail.com>
2010-07-23 00:08:12 +0200

    Some cleaning up of debugs
    Replaced createResponse with send_response

--- WEB-INF/jruby/application.rb
+++ WEB-INF/jruby/application.rb
@@ -5,37 +5,38 @@ SipServletResponse = javax.servlet.sip.SipServletResponse
 
 registrations = {}
 
-Binding = Struct::new(:contact, :call_id, :cseq, :expires)
-
 def canonicalTo(uri)
   "sip:#{uri.user}@#{uri.host.downcase}"
 end
 
-def create_and_send_response(status, msg)
-  request.createResponse(status, msg).send();
-end
-
 register do
+  Binding = Struct::new(:contact, :call_id, :cseq, :expires)
+  class Binding
+    def to_s
+      "Binding[#{contact.to_s}]: call_id: #{call_id}, cseq: #{cseq}, expires: #{expires}"
+    end
+  end
+
   aor = canonicalTo(request.to.uri)
   puts "AOR: #{aor}"
 
   bindings = (registrations[aor] ||= [])
   
   wildcard = false
-  contacts = address_headers[:Contact].map { |contact|
+  contacts = address_headers[:Contact].map do |contact|
     wildcard = true if contact.wildcard?
     contact
-  }
+  end
   if wildcard && contacts.size > 1
-    create_and_send_response SipServletResponse.SC_BAD_REQUEST, "Invalid wildcard" 
+    send_response SipServletResponse.SC_BAD_REQUEST, "Invalid wildcard" 
     break
   end
   cseq = header[:CSEQ].split(/ /).first.to_i
   if wildcard 
     # TODO
-    puts "  WAS WILDCARD"
+    puts "  WAS A WILDCARD"
+    raise RuntimeError, "Not yet implemented"
   else
-    puts "  WAS NOT WILDCARD"
     contacts.each do |contact| 
       puts "  Handling contact: #{contact}"
       expires = contact.expires
@@ -44,16 +45,16 @@ register do
         expires = 300  if expires < 0     # default
         expires = 3600 if expires > 3600  # max expire
         if expires < 60 # min expires
-          response = request.createResponse(SipServletResponse.SC_INTERVAL_TOO_BRIEF)
-          response.addHeader('Min-Expires', 60)
-          response.send
+          send_response SipServletResponse.SC_INTERVAL_TOO_BRIEF do |response|
+            response.addHeader('Min-Expires', 60)
+          end
           break
         end
       end
       binding = bindings.find { |binding| binding.contact == contact.uri }
       if binding
         if (request.call_id == binding.call_id && cseq < binding.cseq)
-          create_and_send_response SipServletResponse.SC_SERVER_INTERNAL_ERROR, "Lower CSeq"
+          send_response SipServletResponse.SC_SERVER_INTERNAL_ERROR, "Lower CSeq"
           break
         end
         if expires == 0
@@ -72,13 +73,11 @@ register do
       end
     end
   end
-  puts bindings.join("\n")
-  response = request.createResponse SipServletResponse.SC_OK
-  response.addHeader('Date', "TODO")
-    
-  # TODO add Contact headers
-    
-  response.send
+  puts "  BINDINGS for #{aor} are #{bindings.map(&:contact).map(&:to_s).join(", ")}"
+  send_response SipServletResponse.SC_OK do |response|
+    response.addHeader('Date', "TODO")
+    # TODO add Contact headers
+  end    
 end
 
 #response do
